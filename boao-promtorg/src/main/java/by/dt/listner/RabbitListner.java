9ef65720f112449636594813b7dee5400107a7d0
Alex Nastin <alexnastin1992@gmail.com>
2017-03-10 19:09:44 +0300

    test rabbitmq commit

---
+++ boao-promtorg/src/main/java/by/dt/listner/RabbitListner.java
@@ -0,0 +1,20 @@
+package by.dt.listner;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitListner {
+
+    @RabbitListener(queues = "query-example-3-1")
+    public void worker1(String message) {
+        System.out.println("PROMTORG accepted on worker 1 : " + message);
+    }
+
+    @RabbitListener(queues = "query-example-3-2")
+    public void worker2(String message) {
+        System.out.println("PROMTORG accepted on worker 2 : " + message);
+    }
+
+}
