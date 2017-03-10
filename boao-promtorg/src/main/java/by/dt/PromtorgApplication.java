11ea932f251d49e295fa12dcf213fb7ed43f1175
Alex Nastin <alexnastin1992@gmail.com>
2017-03-10 13:11:22 +0300

    upd config

--- boao-promtorg/src/main/java/by/dt/PromtorgApplication.java
+++ boao-promtorg/src/main/java/by/dt/PromtorgApplication.java
@@ -1,13 +1,19 @@
 package by.dt;
 
+import by.dt.config.DBConfig;
+import by.dt.config.RabbitMQConfig;
+import by.dt.config.WebConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@ComponentScan("by.dt")
+@Import({DBConfig.class, RabbitMQConfig.class, WebConfig.class})
 public class PromtorgApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PromtorgApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PromtorgApplication.class, args);
+    }
 }
